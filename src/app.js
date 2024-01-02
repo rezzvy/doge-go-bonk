5cb47068bcb193725dd8e7ece987ff63c66d00b3
Muhammad Reza Fahlevi <rezzvy@hotmail.com>
2024-01-02 17:58:23 +0800

    Add a handler for the selected file.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const pointer = document.querySelector(".doge-pointer");
 const bonkAudio = document.querySelector(".bonk-audio");
 
+const bonkFileInput = document.getElementById("bonk-file-input");
+
+const bonkOnImage = document.querySelector(".bonk-on-image");
+const bonkOnVideo = document.querySelector(".bonk-on-video");
+
+const bonkPlayGround = document.getElementById("bonk-playground");
+const appMenu = document.getElementById("app-menu");
+
 function bonk_audio_play() {
   if (bonkAudio.currentTime > 0) {
     bonkAudio.pause();
@@ -25,4 +33,19 @@ window.addEventListener("click", (e) => {
     bonk_audio_play();
     pointer.classList.remove("bonk");
   }, 100);
+});
+
+bonkFileInput.addEventListener("input", (e) => {
+  const file = e.target.files[0];
+  const fileType = file.type.split("/")[0];
+
+  if (fileType === "image") {
+    bonkOnImage.src = URL.createObjectURL(file);
+  } else {
+    bonkOnVideo.src = URL.createObjectURL(file);
+    bonkOnVideo.play();
+  }
+
+  appMenu.classList.add("none");
+  bonkPlayGround.classList.remove("none");
 });
